104cd0abbf60c7b91ebe9dafc4ea264ff5de91ec
Shubham Shastri <103996042+Shubhz02@users.noreply.github.com>
2025-03-10 13:07:16 +0530

    Add files via upload

---
+++ ARM/LCD 16x2 interfacing/lcd_16_2(8 bit).d
@@ -0,0 +1,5 @@
+lcd_16_2(8 bit).o: LCD_16_2(8 BIT).C
+lcd_16_2(8 bit).o: C:\Keil\ARM\Inc\Philips\lpc214x.h
+lcd_16_2(8 bit).o: C:\Keil\ARM\RV31\Inc\stdint.h
+lcd_16_2(8 bit).o: C:\Keil\ARM\RV31\Inc\stdlib.h
+lcd_16_2(8 bit).o: C:\Keil\ARM\RV31\Inc\stdio.h
