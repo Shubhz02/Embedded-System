df47a1efe32e656b31ee10972f00115c8b275369
Shubham Shastri <103996042+Shubhz02@users.noreply.github.com>
2025-03-10 13:18:05 +0530

    Add files via upload

---
+++ ARM/DHT11 Sensor Interfacing/dht11_interfacing.d
@@ -0,0 +1,5 @@
+dht11_interfacing.o: DHT11_INTERFACING.c
+dht11_interfacing.o: C:\Keil\ARM\Inc\Philips\lpc214x.h
+dht11_interfacing.o: C:\Keil\ARM\RV31\Inc\stdint.h
+dht11_interfacing.o: C:\Keil\ARM\RV31\Inc\stdio.h
+dht11_interfacing.o: C:\Keil\ARM\RV31\Inc\string.h
